3b905300de9c00f22f9f9be9e3af466a9e9e4071
Sergio Vazquez <sergiovazquezvarela160bc@gmail.com>
2025-11-01 13:43:23 +0100

    clasificacion y arreglo partidos

--- src/Jornadas.java
+++ src/Jornadas.java
@@ -1,3 +1,57 @@
 public class Jornadas {
 
+
+    private String nombre;
+    private String apellido;
+    private int jornadas;
+    private int puntosindividualesfantasy;
+    private int puntostotalesfantasy ;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getJornadas() {
+        return jornadas;
+    }
+
+    public void setJornadas(int jornadas) {
+        this.jornadas = jornadas;
+    }
+
+    public int getPuntosindividualesfantasy() {
+        return puntosindividualesfantasy;
+    }
+
+    public void setPuntosindividualesfantasy(int puntosindividualesfantasy) {
+        this.puntosindividualesfantasy = puntosindividualesfantasy;
+    }
+
+    public int getPuntostotalesfantasy() {
+        return puntostotalesfantasy;
+    }
+
+    public void setPuntostotalesfantasy(int puntostotalesfantasy) {
+        this.puntostotalesfantasy = puntostotalesfantasy;
+    }
+    public Jornadas(String nombre, String apellido, int jornadas, int puntosindividualesfantasy, int puntostotalesfantasy) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.jornadas = jornadas;
+        this.puntosindividualesfantasy = puntosindividualesfantasy;
+        this.puntostotalesfantasy = puntostotalesfantasy;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         int opcion;
+        int[] pindvtotal;
+        int[] pequiptotal;
+        int jornadas = 0;
+
+        Jornadas[] ptotal = new Jornadas[12];
+        for (int i = 0; i < 12; i++) {
+            ptotal[i] = new Jornadas("y", "x", 0, 0, 0);
+
+        }
+
         // PROYECTO FANTASY CATEGORIAS BASE SIN REBOTES ROBOS ASISTENCIAS ETC
         //START MENU
         do {
@@ -17,12 +27,17 @@ public class Main {
                 opcion = doMenu();
 
             }
+
             switch (opcion) {
                 case 1:
                     System.out.println("no implementado");
                     break;
                 case 2:
                     System.out.println("no implementado");
+                    System.out.println("\n=== ESTE ES EL RESUMEN DE TU EQUIPO FANTASY ==");
+                    for (int i = 0; i < 12; i++) {
+                        System.out.println(" jugador "+ (i+1) + " con nombre "+ ptotal[i].getNombre() +" puntos individuales ==> "+ ptotal[i].getPuntosindividualesfantasy() + " Y puntos de equipo ==> " + ptotal[i].getPuntostotalesfantasy());
+                    }
                     break;
                 case 3:
                     System.out.println("no implementado");
@@ -33,17 +48,39 @@ public class Main {
                     int jugadores = entrada.nextInt();
 
                     Jugador[] personas = new Jugador[jugadores];
+                    for (int i = 0; i < jugadores; i++) {
+
+
+                        personas[i] = new Jugador("y", "x", 0, 0, 0, 0, 0, 0);
+                    }
 
 
                     // datos de un partido
                     for (int i = 0; i < personas.length; i++) {
-                        personas[i] = new Jugador("x", "x", 0, 0, 0, 0, 0, 0);
+                        int pruebacontrol = 0;
+
 
                         System.out.println("=== CÁLCULO DE PUNTAJE DE JUGADOR ===");
                         System.out.println("Nombre del jugador " + (i + 1) + " : ");
                         personas[i].setNombre(entrada.next());
+
+                        for (int i1 = 0; i1 < ptotal.length; i1++) {
+                            if (personas[i].getNombre().equals(ptotal[i1].getNombre())) {
+
+                                pruebacontrol = i1;
+                                break;
+
+
+                            } else if (ptotal[i1].getNombre().equals("y")) {
+                                pruebacontrol = i1;
+                                ptotal[i1].setNombre(personas[i].getNombre());
+                                break;
+                            }
+                        }
+
                         System.out.println("Apellido del jugador: ");
                         personas[i].setApellido(entrada.next());
+                        ptotal[pruebacontrol].setApellido(personas[i].getApellido());
 
                         System.out.print("Puntos anotados: ");
                         personas[i].setPuntos(entrada.nextInt());
@@ -58,59 +95,78 @@ public class Main {
 
 
                         personas[i].setPuntosindividuales(personas[i].getPuntos() * 3 + personas[i].getMinutos() - personas[i].getPenalizacionFaltas() * 2);
+                        ptotal[pruebacontrol].setPuntosindividualesfantasy(ptotal[pruebacontrol].getPuntosindividualesfantasy() + personas[i].getPuntosindividuales());
 
 
-                    }
-
-
-                    // Datos colectivos
-                    System.out.println("\n=== RESULTADO DEL EQUIPO ===");
-                    System.out.print("¿El equipo ganó o perdió? (g/p): ");
-                    char resultado = entrada.next().charAt(0);
-                    while (resultado != 'g' && resultado != 'p' && resultado != 'P' && resultado != 'G') {
-                        System.out.println("Error introduce bien g si gananron el partido y p si perdieron el partido : ");
-                        resultado = entrada.next().charAt(0);
-                    }
-                    System.out.print("Diferencia de puntos en el marcador: ");
-                    int diferencia = entrada.nextInt();
-
-                    int puntosEquipo = 0;
-
-                    if (resultado == 'g') {
-                        if (diferencia > 20) {
-                            puntosEquipo = 10;
-                        } else {
-                            puntosEquipo = 5;
+                        // Datos colectivos
+                        System.out.println("\n=== RESULTADO DEL EQUIPO ===");
+                        System.out.print("¿El equipo ganó o perdió? (g/p): ");
+                        char resultado = entrada.next().charAt(0);
+                        while (resultado != 'g' && resultado != 'p' && resultado != 'P' && resultado != 'G') {
+                            System.out.println("Error introduce bien g si gananron el partido y p si perdieron el partido : ");
+                            resultado = entrada.next().charAt(0);
                         }
-                    } else if (resultado == 'p') {
-                        if (diferencia > 20) {
-                            puntosEquipo = -10;
+                        System.out.print("Diferencia de puntos en el marcador: ");
+                        int diferencia = entrada.nextInt();
+
+                        int puntosEquipo = 0;
+
+                        if (resultado == 'g') {
+                            if (diferencia > 20) {
+                                puntosEquipo = 10;
+                            } else {
+                                puntosEquipo = 5;
+                            }
+                        } else if (resultado == 'p') {
+                            if (diferencia > 20) {
+                                puntosEquipo = -10;
+                            } else {
+                                puntosEquipo = -5;
+                            }
                         } else {
-                            puntosEquipo = -5;
+                            System.out.println("Entrada inválida. No se sumarán puntos de equipo.");
                         }
-                    } else {
-                        System.out.println("Entrada inválida. No se sumarán puntos de equipo.");
+                        personas[i].setPuntostotales(personas[i].getPuntosindividuales() + puntosEquipo);
+                        ptotal[pruebacontrol].setPuntostotalesfantasy(ptotal[pruebacontrol].getPuntostotalesfantasy() + personas[i].getPuntostotales());
                     }
+
 
                     // Puntaje total final
 
                     //for (int i = 0; i < personas.length; i++) {
                     //    personas[i].setPuntostotales(personas[i].getPuntosindividuales()+puntosEquipo);
-                    calculoindividual(personas, puntosEquipo);
+
+
                     //}
 
                     System.out.println("\n=== RESULTADO FANTASY ===");
                     for (int i = 0; i < personas.length; i++) {
+                        int pruebacontrol = 0;
+                        for (int i1 = 0; i1 < ptotal.length; i1++) {
+
+                            if (personas[i].getNombre().equals(ptotal[i1].getNombre())) {
+
+                                pruebacontrol = i1;
+                            }
+
+
+                        }
+
+
                         System.out.print("\njugador: " + (i + 1) + " con nombre : " + personas[i].getNombre() + " ANOTÓ ==> " + personas[i].getPuntosindividuales() + " puntos fantasy individuales");
                         System.out.print(" y puntos FANTASY DE EQUIPO ==> " + personas[i].getPuntostotales());
+                        System.out.print(" \npuntos acumulados individuales==> " + ptotal[pruebacontrol].getPuntosindividualesfantasy() + " puntos equipo Fnatsay acumulados ==> " + ptotal[pruebacontrol].getPuntostotalesfantasy());
 
                     }
-                    System.out.println(" ================ ");
+                    System.out.println("\n ================ ");
+                    jornadas++;
+                    break;
+
 
             }
+        }while (opcion != 5) ;
 
 
-        } while (opcion != 5);
     }
 
     public static int doMenu() {
@@ -121,11 +177,11 @@ public class Main {
         Scanner menuselect = new Scanner(System.in);
         System.out.println("\n--<=== BIENVENIDO AL MENU FANTASY ===>--");
         System.out.println("\n pulsa el numero correspondiente para acceder a las secciones");
-        System.out.println("- 1. MERCADO(SIN ACABAR).");
-        System.out.println("- 2. TU EQUIPO FANTASY(SIN ACABAR) ");
-        System.out.println("- 3. CLASIFICACION FANTASY(SIN ACABAR) ");
+        System.out.println("- 1. MERCADO(NO IMPLEMENTADO).");
+        System.out.println("- 2. TU EQUIPO FANTASY(FUNCIONA A MEJORAR) ");
+        System.out.println("- 3. CLASIFICACION FANTASY(NO IMPLEMENTADO) ");
         System.out.println("- 4. PARTIDO MANUAL(FUNCIONA)");
-        System.out.println("- 5. SALIR DE LA APLICACCION(FUNCIONA) ");
+        System.out.println("- 5. SALIR DE LA APLICACCION (FUNCIONA) ");
         System.out.println(" etc work in progress... ");
         System.out.println("TU SELECCION =>");
 
@@ -133,13 +189,7 @@ public class Main {
 
         return menu;
     }
-    public static void calculoindividual (Jugador[]players,int x){
-        for (int i = 0; i < players.length; i++) {
 
-
-            players[i].setPuntostotales(players[i].getPuntosindividuales() + x);
-        }
-    }
 
 
 
